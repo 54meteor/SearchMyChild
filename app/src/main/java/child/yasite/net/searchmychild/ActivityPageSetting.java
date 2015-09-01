cec3d5532b44837e1a3c33b76de049c43dc9785b
Administrator <liupeng@yasite.net>
2015-09-01 16:28:59 +0800

    Initial commit

---
+++ app/src/main/java/child/yasite/net/searchmychild/ActivityPageSetting.java
@@ -0,0 +1,10 @@
+package child.yasite.net.searchmychild;
+
+import android.os.Bundle;
+
+public interface ActivityPageSetting {
+	public void setupView(Bundle arg0);
+	public void setContent();
+	public void setModel();
+	public boolean getIntentValue();
+}
---
+++ app/src/main/java/child/yasite/net/searchmychild/MainActivity.java
@@ -0,0 +1,31 @@
+package child.yasite.net.searchmychild;
+
+
+
+import android.os.Bundle;
+
+
+public class MainActivity extends BaseNewActivity {
+
+
+
+    @Override
+    public void setupView(Bundle arg0) {
+
+    }
+
+    @Override
+    public void setContent() {
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void setModel() {
+
+    }
+
+    @Override
+    public boolean getIntentValue() {
+        return true;
+    }
+}
